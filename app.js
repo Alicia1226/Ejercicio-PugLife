9dd7dae564cf13609f75fa3da5f74661fdba92db
Alicia1226 <alicia.verdugo.payo@gmail.com>
2021-05-31 17:33:56 +0200

    cambios

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const app = express()
 const router= require('./controllers/index')
 const port = 3001
 
-app.use('/',router);
+app.use("/public",express.static('public'));
+
 
 app.set('view engine', 'pug');
 app.set('views','./views');
-
-
-
-
-
-
-
-
-
-
+app.use('/',router);
       
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
--- controllers/pages.js
+++ controllers/pages.js
@@ -3,26 +3,29 @@ const pages = {
         // res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
         // res.send('Es la home!')
         let datos = {nombre:"Alicia"}
-        res.status(200).render('home',datos);
+        res.status(200).render('home');
     },
     
     about:(req, res) => {
-        // res.status(200).render('Esto es about!')
+
         
-        res.status(200).send('Esto es about!')
+        res.status(200).render('about')
+        
+        // res.status(200).send('Esto es about!')
     },
    contact:(req, res) => {
-        // res.status(200).render('Esto es contact!')
+        res.status(200).render('contact')
 
-        res.status(200).send('Esto es contact!')
+        // res.status(200).send('Esto es contact!')
     },
     location:(req, res) => {
-        res.status(200).send('Esto es location!')
+        // res.status(200).send('Esto es location!')
+        res.status(200).render('location')
       },
     mission:(req, res) => {
-        // res.status(200).render('Esto es mission!')
+        res.status(200).render('mission')
 
-        res.status(200).send('Esto es mission!')
+        // res.status(200).send('Esto es mission!')
 
       },
 
