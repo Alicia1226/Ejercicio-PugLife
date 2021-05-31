5f678436cd955b58665b36769305678d87f39c82
Alicia1226 <alicia.verdugo.payo@gmail.com>
2021-05-31 15:46:03 +0200

    nuevos cambios

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const router= require('./controllers/index')
+const port = 3001
 
 
-
-
+app.use('/',router);
 
 
 
--- controllers/pages.js
+++ controllers/pages.js
@@ -1,22 +1,29 @@
 const pages = {
     home:(req, res) => {
         // res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
-        //res.send('Es la home!')
+        res.send('Es la home!')
         let datos = {nombre:"Alicia"}
-        res.status(200).render('home',datos);
+        // res.status(200).render('home',datos);
     },
     
     about:(req, res) => {
-        res.status(200).render('Esto es about!')
+        // res.status(200).render('Esto es about!')
+        
+        res.status(200).send('Esto es about!')
     },
    contact:(req, res) => {
-        res.status(200).render('Esto es contact!')
+        // res.status(200).render('Esto es contact!')
+
+        res.status(200).send('Esto es contact!')
     },
     location:(req, res) => {
         res.status(200).send('Esto es location!')
       },
     mission:(req, res) => {
-        res.status(200).render('Esto es mission!')
+        // res.status(200).render('Esto es mission!')
+
+        res.status(200).send('Esto es mission!')
+
       },
 
 }
