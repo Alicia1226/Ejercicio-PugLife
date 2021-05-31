af84a76f3b94a61c9ddcee9332d45bf7b51b0695
VictorMaVargas <victormavargas@gmail.com>
2021-05-31 17:33:50 +0200

    images y script

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,22 +1,22 @@
 const pages = {
     home: (req, res) => {
         //res.send('Esto es home')
-        //let datos = {nombre:"Victor"}
-        res.status(200).render("home")
+        let datos = {nombre:"Victor"}
+        res.status(200).render("home",datos)
     },
     location: (req, res) => {
-        res.status(200).send('Esto es location')
+        res.status(200).render('location')
         },
     about: (req, res) => {
-            res.status(200).send('Esto es about')
+        res.status(200).render('about')
             },
     contact: (req, res) => {
         //res.status(200).send('Esto es contact')
         let datos = {nombre:"al formulario de contacto"}
-        res.status(200).render('home', datos)
+        res.status(200).render('contact', datos)
         },
     mission: (req, res) => {
-        res.status(200).send('Esto es mission')
+        res.status(200).render('mission')
             },
 
     }
